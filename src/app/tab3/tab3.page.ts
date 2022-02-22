3ff27efbf07f02820186b37286b27fc21fbe7d83
unknown <abril.hernandez.hernandez.02@gmail.com>
2022-02-21 20:55:40 -0600

    Primer proyecto Ionic-Angular presentación

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+  select : string = ""
+  facebook : string=''
+  instagram : string=''
+  whats : String=''
   constructor() {}
 
 }
